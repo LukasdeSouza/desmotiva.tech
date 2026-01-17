d916ec2316a06ac92c82e03347f02c372bfa73f9
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2026-01-17 02:39:51 -0300

    chore: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const poppins = Poppins({
@@ -112,6 +113,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics/>
     </html>
   );
 }
